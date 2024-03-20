5eb2b31177a8cd6d26a0f8b1ed8b70ba72fa06e4
katrinDev <polozkovak8@gmail.com>
2024-03-20 09:56:05 +0300

    add desktop left content part styles

--- task3-youtube/video-player-page/src/App.tsx
+++ task3-youtube/video-player-page/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ContentContainer from "./components/content/ContentContainer";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <ContentContainer />
     </>
   );
 }
---
+++ task3-youtube/video-player-page/src/components/content/VideoList.tsx
@@ -0,0 +1,5 @@
+import "../../styles/VideoList.css";
+
+export default function VideoList() {
+  return <div id="video-list"></div>;
+}
